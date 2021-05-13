6c616ad12addf758b3001246ff72bdcb60392db9
Mahesh471 <mahesh.nagpure03@gmail.com>
2021-05-13 16:05:42 +0530

    Welcome Screen UI added

--- FamilyTracker/Shared/Extensions/Storyboard+Extension.swift
+++ FamilyTracker/Shared/Extensions/Storyboard+Extension.swift
@@ -16,7 +16,7 @@ extension UIStoryboard {
             case .MainTvOS:
                 return "MainTvOS"
             case .MainiOS:
-                return "Main"
+                return "SignIn"
             }
         }
     }
@@ -46,7 +46,7 @@ extension UIStoryboard {
     class var sharedInstance: UIStoryboard {
         struct Singleton {
             #if os(iOS)
-            static let instance = UIStoryboard(name: "Main", bundle: nil)
+            static let instance = UIStoryboard(name: "SignIn", bundle: nil)
             #elseif os(tvOS)
             static let instance = UIStoryboard(name: "Main_tvOS", bundle: nil)
             #endif
